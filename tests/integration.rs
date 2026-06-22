39d13161774fdcae204a141590df08bf676ac9e1
yoav <yoav@phantomic.live>
2026-06-22 00:16:06 -0400

    fix(login): clippy uninlined-format-args in test helper

--- tests/integration.rs
+++ tests/integration.rs
@@ -1651,7 +1651,7 @@ mod login_tests {
                 b'A'..=b'Z' | b'a'..=b'z' | b'0'..=b'9' | b'-' | b'_' | b'.' | b'~' => {
                     (b as char).to_string()
                 }
-                _ => format!("%{:02X}", b),
+                _ => format!("{b:02X}"),
             })
             .collect()
     }
